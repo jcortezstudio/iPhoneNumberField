16d57c61e37f529c05a34e6d2b2543661ece7662
Juan Carlos Cortez Lopez <jcortezstudio@MacBook-Air-de-Juan.local>
2023-06-02 10:49:31 -0600

    Fix bugs

--- Sources/iPhoneNumberField/iPhoneNumberField.swift
+++ Sources/iPhoneNumberField/iPhoneNumberField.swift
@@ -174,6 +174,7 @@ public struct iPhoneNumberField: UIViewRepresentable {
         uiView.withFlag = showFlag
         uiView.withDefaultPickerUI = selectableFlag
         uiView.withPrefix = previewPrefix
+        uiView.partialFormatter.defaultRegion = defaultRegion ?? PhoneNumberKit.defaultRegionCode()
         if placeholder != nil {
             uiView.placeholder = placeholder
         } else {
@@ -192,10 +193,12 @@ public struct iPhoneNumberField: UIViewRepresentable {
             uiView.textAlignment = textAlignment
         }
 
-        if isFirstResponder {
-            uiView.becomeFirstResponder()
-        } else {
-            uiView.resignFirstResponder()
+        DispatchQueue.main.async {
+            if isFirstResponder {
+                uiView.becomeFirstResponder()
+            } else {
+                uiView.resignFirstResponder()
+            }
         }
         
         configuration(uiView)
